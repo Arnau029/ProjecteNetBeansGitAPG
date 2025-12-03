56ea5c8eaffe1f880098b828fdcd91787567c64c
Arnau029 <arpagr@jviladoms.cat>
2025-12-03 10:48:42 +0100

    primer commit

---
+++ src/main/java/com/mycompany/projectenetbeansgitapg/ProjecteNetBeansGitAPG.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projectenetbeansgitapg;
+
+/**
+ *
+ * @author asher
+ */
+public class ProjecteNetBeansGitAPG {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
